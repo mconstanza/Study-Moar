67d72d5c972cb6e9fc96c4c0c4156b27d6362ba6
Mike Constanza <michael.constanza@gmail.com>
2017-02-09 11:34:47 -0500

    pushing heroku build

---
+++ client/src/components/CardContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CardContainer extends Component {
+  render() {
+    return (
+      <div id="CardContainer">
+        
+      </div>
+    );
+  }
+}
+
+export default CardContainer;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Search from './Search';
+import CardContainer from './CardContainer';
 
 class Home extends Component {
   render() {
     return (
       <div id="Home">
         <Search/>
+        <CardContainer/>
       </div>
     );
   }
